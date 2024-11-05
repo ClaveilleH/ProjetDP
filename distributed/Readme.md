e26c22bbbfc75adb04388476960bb9c6f9f3b164
XU Chuan <chuan.xu@inria.fr>
2024-11-05 21:45:59 +0100

    new

--- distributed/Readme.md
+++ distributed/Readme.md
@@ -1,2 +1,5 @@
 ## Start
 - `torchrun --nnodes=1 --nproc-per-node=2 launch.py`
+
+## Questions
+1. Check the code of launch.py, what are the functions to get the rank of the process and the number of all processes?
--- distributed/launch.py
+++ distributed/launch.py
@@ -1,5 +1,3 @@
 import torch.distributed as dist
-import os
-os.environ["OMP_NUM_THREADS"] = "1"
 dist.init_process_group(backend='gloo', init_method="env://")
 print('Hello from process {} (out of {})!'.format(dist.get_rank(), dist.get_world_size()))
