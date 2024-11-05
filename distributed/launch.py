703c40b7d5fb3167136ce8065da05135c449751f
XU Chuan <chuan.xu@inria.fr>
2024-11-05 21:40:05 +0100

    update

--- distributed/launch.py
+++ distributed/launch.py
@@ -1,3 +1,5 @@
 import torch.distributed as dist
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
 dist.init_process_group(backend='gloo', init_method="env://")
 print('Hello from process {} (out of {})!'.format(dist.get_rank(), dist.get_world_size()))
