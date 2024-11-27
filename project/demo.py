533871b7dfea4ee24dd3b196e92675bd5f727a98
XU Chuan <chuan.xu@inria.fr>
2024-11-27 07:05:29 +0100

    update

--- project/demo.py
+++ project/demo.py
@@ -3,6 +3,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 import time
+import torchvision.transforms as transforms
 import torchvision.models as models
 import torchvision
 from torch.utils.data import DataLoader
@@ -11,14 +12,21 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 
 def run(rank, size):
-
-    dataset = torchvision.datasets.Imagenette('/data/coati/user/tdasilva/dataset/imagenet64', download=False)
+    # Define transformations
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    dataset = torchvision.datasets.Imagenette('/data/neo/user/chxu/', transform=transform_train)
     dataset_size = len(dataset)
     localdataset_size = dataset_size//size
     local_dataset = torch.utils.data.Subset(dataset, range(rank*localdataset_size, (rank+1)*localdataset_size))
     sample_size = 1024//size
     dataloader = DataLoader(local_dataset, batch_size=sample_size, shuffle=True)
     model = models.resnet18()
+    model.fc = nn.Linear(model.fc.in_features, len(dataset.classes))
     ddp_model = DDP(model)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
