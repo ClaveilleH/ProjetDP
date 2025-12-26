43ddc7bb752c17ae331b99bd578a97f3e6953231
Hugo <h.claveille@gmail.com>
2025-12-26 16:09:08 +0100

    eedit sh

--- project/run_gpu.sh
+++ project/run_gpu.sh
@@ -1,5 +1,9 @@
 module load conda
 conda activate ~/.conda/envs/pytorch/
-torchrun --nnodes=1 --nproc-per-node=1 demo_gpu.py
-echo "<----------------------------------->"
-torchrun --nnodes=1 --nproc-per-node=2 demo_gpu.py
+# torchrun --nnodes=1 --nproc-per-node=1 demo_gpu.py
+# echo "<----------------------------------->"
+# torchrun --nnodes=1 --nproc-per-node=2 demo_gpu.py
+
+torchrun --nnodes=1 --nproc-per-node=4 demo.py
+# echo "<----------------------------------->"
+# torchrun --nnodes=1 --nproc-per-node=8 demo_gpu.py
