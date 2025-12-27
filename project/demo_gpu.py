787832c0e8f945c6813745d1fa4ec3c1eeb594b0
Hugo <h.claveille@gmail.com>
2025-12-27 18:48:46 +0100

    modif gpu

--- project/demo_gpu.py
+++ project/demo_gpu.py
@@ -15,6 +15,7 @@ import tarfile
 
 
 def run(rank, size):
+    
     torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
     # create model and move it to GPU with id rank
     device_id = rank % torch.cuda.device_count()
@@ -61,17 +62,24 @@ def run(rank, size):
     train_labels = train_labels.to(device_id)
     et_read = time.time()
     print(f'Loading time: {et_read-st} seconds')
+    loading_time = et_read - st
     optimizer.zero_grad()
     outputs = ddp_model(train_images)
     loss_fn(outputs, train_labels).backward()
     et = time.time()
     print(f'Computing + Communication time: {et-et_read} seconds')
+    computing_time = et - et_read
     optimizer.step()
     dist.destroy_process_group()
     print(f"Finished running basic DDP example on rank {rank}.")
-
+    return loading_time, computing_time
+    
 if __name__ == "__main__":
     dist.init_process_group("nccl", init_method="env://")
     size = dist.get_world_size()
     rank = dist.get_rank()
-    run(rank, size)
+
+    loading_time, computing_time = run(rank, size)
+    # on stocke le resultat dans un fichier csv pour pouvoir le traiter plus tard
+    with open("results/results_gpu.csv", "a") as f:
+        f.write(f"{rank};{size};{loading_time};{computing_time};{loading_time+computing_time}\n")
