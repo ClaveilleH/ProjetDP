bf8173620fa51305cad7dd1fbe2b44fd812dfa34
Hugo <h.claveille@gmail.com>
2026-01-03 10:01:59 +0100

    8cpu

--- project/run.sh
+++ project/run.sh
@@ -12,35 +12,43 @@ SCRIPT="cpu.py"
 # SCRIPT_ARGS="${@:2}"
 SCRIPT_ARGS=""
 
-echo "Running script: $SCRIPT $SCRIPT_ARGS with NPROC_PER_NODE=$NPROC_PER_NODE"
-rm -f results/results_cpu.csv
-echo "size;batch_size;total_loading_time;total_computing_time;total_time\n" >> results/results_cpu.csv
+# echo "Running script: $SCRIPT $SCRIPT_ARGS with NPROC_PER_NODE=$NPROC_PER_NODE"
+# rm -f results/results_cpu.csv
+# echo "size;batch_size;total_loading_time;total_computing_time;total_time\n" >> results/results_cpu.csv
 
 
 
 for batch_size in 16 32 64 128 256
 do
-    torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=1 "$SCRIPT" "$batch_size"
 
-    torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=2 "$SCRIPT" "$batch_size"
 
-    torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=3 "$SCRIPT" "$batch_size"
 
-    torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=4 "$SCRIPT" "$batch_size"
 
-    torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=5 "$SCRIPT" "$batch_size"
 
-    torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
-    torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
+    # torchrun --nnodes=1 --nproc-per-node=6 "$SCRIPT" "$batch_size"
+
+    torchrun --nnodes=1 --nproc-per-node=7 "$SCRIPT" "$batch_size"
+    torchrun --nnodes=1 --nproc-per-node=7 "$SCRIPT" "$batch_size"
+    torchrun --nnodes=1 --nproc-per-node=7 "$SCRIPT" "$batch_size"
+
+    torchrun --nnodes=1 --nproc-per-node=8 "$SCRIPT" "$batch_size"
+    torchrun --nnodes=1 --nproc-per-node=8 "$SCRIPT" "$batch_size"
+    torchrun --nnodes=1 --nproc-per-node=8 "$SCRIPT" "$batch_size"
 done
